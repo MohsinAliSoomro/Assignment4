29190ae8b567e94d6035c6f261f9d36444639b74
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-05-23 05:56:52 +0500

    Carousel is added successfully

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,21 @@
-console.log("initial value")
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:20,
+        autoplay:true,
+        dots:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2,
+            },
+            600:{
+                items:4,
+            },
+            1000:{
+                items:6,                
+            }
+        }
+    });
+
+  });
